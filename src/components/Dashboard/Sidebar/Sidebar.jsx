1ea0cd3eed20031389dab734b2d0dc32f455bc1f
smile995 <ah.smile995@gmail.com>
2025-02-09 22:26:35 +0600

    Make the route secure based on user role

--- src/components/Dashboard/Sidebar/Sidebar.jsx
+++ src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -1,39 +1,41 @@
 /* eslint-disable no-unused-vars */
-import { useState } from 'react'
-import { GrLogout } from 'react-icons/gr'
-import { FcSettings } from 'react-icons/fc'
+import { useState } from "react";
+import { GrLogout } from "react-icons/gr";
+import { FcSettings } from "react-icons/fc";
 
-import { BsFingerprint,BsFillHouseAddFill } from 'react-icons/bs'
-import { GrUserAdmin } from 'react-icons/gr'
-import { MdHomeWork } from 'react-icons/md'
-import { AiOutlineBars } from 'react-icons/ai'
-import { BsGraphUp } from 'react-icons/bs'
-import { NavLink } from 'react-router-dom'
-import useAuth from '../../../hooks/useAuth'
-import { Link } from 'react-router-dom'
-import MenuItem from '../../Shared/Navlink/MenuItem'
+import { BsFingerprint, BsFillHouseAddFill } from "react-icons/bs";
+import { GrUserAdmin } from "react-icons/gr";
+import { MdHomeWork } from "react-icons/md";
+import { AiOutlineBars } from "react-icons/ai";
+import { BsGraphUp } from "react-icons/bs";
+import { NavLink } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
+import { Link } from "react-router-dom";
+import MenuItem from "../../Shared/Navlink/MenuItem";
+import useRole from "../../../hooks/useRole";
 
 const Sidebar = () => {
-  const { logOut } = useAuth()
-  const [isActive, setActive] = useState(false)
+  const { logOut } = useAuth();
+  const [isActive, setActive] = useState(false);
+  const [role] = useRole();
 
   // Sidebar Responsive Handler
   const handleToggle = () => {
-    setActive(!isActive)
-  }
+    setActive(!isActive);
+  };
   return (
     <>
       {/* Small Screen Navbar */}
-      <div className='bg-gray-100 text-gray-800 flex justify-between md:hidden'>
+      <div className="bg-gray-100 text-gray-800 flex justify-between md:hidden">
         <div>
-          <div className='block cursor-pointer p-4 font-bold'>
-            <Link to='/'>
+          <div className="block cursor-pointer p-4 font-bold">
+            <Link to="/">
               <img
                 // className='hidden md:block'
-                src='https://i.ibb.co/4ZXzmq5/logo.png'
-                alt='logo'
-                width='100'
-                height='100'
+                src="https://i.ibb.co/4ZXzmq5/logo.png"
+                alt="logo"
+                width="100"
+                height="100"
               />
             </Link>
           </div>
@@ -41,45 +43,81 @@ const Sidebar = () => {
 
         <button
           onClick={handleToggle}
-          className='mobile-menu-button p-4 focus:outline-none focus:bg-gray-200'
+          className="mobile-menu-button p-4 focus:outline-none focus:bg-gray-200"
         >
-          <AiOutlineBars className='h-5 w-5' />
+          <AiOutlineBars className="h-5 w-5" />
         </button>
       </div>
 
       {/* Sidebar */}
       <div
         className={`z-10 md:fixed flex flex-col justify-between overflow-x-hidden bg-gray-100 w-64 space-y-6 px-2 py-4 absolute inset-y-0 left-0 transform ${
-          isActive && '-translate-x-full'
+          isActive && "-translate-x-full"
         }  md:translate-x-0  transition duration-200 ease-in-out`}
       >
         <div>
           <div>
-            <div className='w-full hidden md:flex px-4 py-2 shadow-lg rounded-lg justify-center items-center bg-rose-100 mx-auto'>
-              <Link to='/'>
+            <div className="w-full hidden md:flex px-4 py-2 shadow-lg rounded-lg justify-center items-center bg-rose-100 mx-auto">
+              <Link to="/">
                 <img
                   // className='hidden md:block'
-                  src='https://i.ibb.co/4ZXzmq5/logo.png'
-                  alt='logo'
-                  width='100'
-                  height='100'
+                  src="https://i.ibb.co/4ZXzmq5/logo.png"
+                  alt="logo"
+                  width="100"
+                  height="100"
                 />
               </Link>
             </div>
           </div>
 
           {/* Nav Items */}
-          <div className='flex flex-col justify-between flex-1 mt-6'>
+          <div className="flex flex-col justify-between flex-1 mt-6">
             {/* Conditional toggle button here.. */}
 
             {/*  Menu Items */}
             <nav>
-              {/* Statistics */}
-              <MenuItem icon={BsGraphUp} text='Statistics' to='/dashboard' />
-              <MenuItem icon={BsFillHouseAddFill} text='Add Room' to='add-room' />
-              <MenuItem icon={MdHomeWork} text='My Listings' to='my-listings' />
-         
-
+              {role === "guest" && (
+                <>
+                  <MenuItem
+                    icon={BsGraphUp}
+                    text="Statistics"
+                    to="/dashboard"
+                  />
+                </>
+              )}
+              {role === "admin" && (
+                <>
+                  <MenuItem
+                    icon={GrUserAdmin}
+                    text="Manage Users"
+                    to="manage-users"
+                  />
+                  <MenuItem
+                    icon={BsGraphUp}
+                    text="Statistics"
+                    to="/dashboard"
+                  />
+                </>
+              )}
+              {role === "host" && (
+                <>
+                  <MenuItem
+                    icon={BsFillHouseAddFill}
+                    text="Add Room"
+                    to="add-room"
+                  />
+                  <MenuItem
+                    icon={BsGraphUp}
+                    text="Statistics"
+                    to="/dashboard"
+                  />
+                  <MenuItem
+                    icon={MdHomeWork}
+                    text="My Listings"
+                    to="my-listings"
+                  />
+                </>
+              )}
             </nav>
           </div>
         </div>
@@ -89,29 +127,29 @@ const Sidebar = () => {
 
           {/* Profile Menu */}
           <NavLink
-            to='/dashboard/profile'
+            to="/dashboard/profile"
             className={({ isActive }) =>
               `flex items-center px-4 py-2 my-5  transition-colors duration-300 transform  hover:bg-gray-300   hover:text-gray-700 ${
-                isActive ? 'bg-gray-300  text-gray-700' : 'text-gray-600'
+                isActive ? "bg-gray-300  text-gray-700" : "text-gray-600"
               }`
             }
           >
-            <FcSettings className='w-5 h-5' />
+            <FcSettings className="w-5 h-5" />
 
-            <span className='mx-4 font-medium'>Profile</span>
+            <span className="mx-4 font-medium">Profile</span>
           </NavLink>
           <button
             onClick={logOut}
-            className='flex w-full items-center px-4 py-2 mt-5 text-gray-600 hover:bg-gray-300   hover:text-gray-700 transition-colors duration-300 transform'
+            className="flex w-full items-center px-4 py-2 mt-5 text-gray-600 hover:bg-gray-300   hover:text-gray-700 transition-colors duration-300 transform"
           >
-            <GrLogout className='w-5 h-5' />
+            <GrLogout className="w-5 h-5" />
 
-            <span className='mx-4 font-medium'>Logout</span>
+            <span className="mx-4 font-medium">Logout</span>
           </button>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
