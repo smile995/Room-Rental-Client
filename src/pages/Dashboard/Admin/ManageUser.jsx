d1fef1bb9b3805cd73eaa65d22443352f4f8b796
smile995 <ah.smile995@gmail.com>
2025-02-12 12:44:23 +0600

    Facing more error and trying to fixed

--- src/pages/Dashboard/Admin/ManageUser.jsx
+++ src/pages/Dashboard/Admin/ManageUser.jsx
@@ -1,20 +1,45 @@
 import { Helmet } from "react-helmet-async";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { useState } from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
-import { useQuery } from "@tanstack/react-query";
-
 
 const ManageUser = () => {
   const axiosSecure = useAxiosSecure();
-
   const { data: users = [], refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const { data } = await axiosSecure.get("/users");
-      return data;
+      return Array.isArray(data) ? data : [];
     },
   });
-  const handleUserRole = (curentUser) => {
-    console.log(curentUser);
+  // const users = [];
+  // console.log(ajaira);
+
+  // const [newRole, setNewRole] = useState("guest");
+  // const [userInfo, setUserInfo] = useState();
+
+  // const { mutateAsync } = useMutation({
+  //   mutationKey: [newRole, userInfo?.email],
+  //   mutationFn: async (role) => {
+  //     const { data } = await axiosSecure.patch(
+  //       `/users/update-role/${userInfo?.email}`,
+  //       role
+  //     );
+  //     return data;
+  //   },
+  //   onSuccess: () => {
+  //     refetch();
+  //     toast.success("User role updated successfully");
+  //   },
+  //   onError: (error) => {
+  //     toast.error(error.message);
+  //   },
+  // });
+  const handleUserRole = async (e) => {
+    e.preventDefault();
+    // await mutateAsync({newRole});
+    // document.getElementById("my_modal_1").close();
   };
   return (
     <>
@@ -72,12 +97,12 @@ const ManageUser = () => {
                         {user?.status ? (
                           <p
                             className={`${
-                              user.status === "verified"
+                              user?.status === "verified"
                                 ? "text-green-500 capitalize "
                                 : "text-yellow-500 capitalize"
                             } whitespace-no-wrap`}
                           >
-                            {user.status}
+                            {user?.status}
                           </p>
                         ) : (
                           <p className="text-red-500 whitespace-no-wrap">
@@ -89,18 +114,51 @@ const ManageUser = () => {
                       <td className="px-5 py-5 border-b border-gray-200  text-sm">
                         <button
                           disabled={user?.role === "admin"}
-                          onClick={() => handleUserRole(user)}
                           className={`relative inline-block px-3 py-1 font-semibold leading-tight rounded-full 
-                                  ${
-                                    user?.role === "admin"
-                                      ? "bg-red-500 text-white opacity-50 cursor-not-allowed"
-                                      : "bg-green-200 text-green-900"
-                                  }`}
+                            ${
+                              user?.role === "admin"
+                                ? "bg-red-500 text-white opacity-50 cursor-not-allowed"
+                                : "bg-green-200 text-green-900"
+                            }`}
+                          onClick={() => {
+                            // setUserInfo(user);
+                            document.getElementById("my_modal_1").showModal();
+                          }}
                         >
                           Update Role
                         </button>
-
-                        {/* Update User Modal */}
+                        <dialog id="my_modal_1" className="modal">
+                          <div className="modal-box">
+                            <h3 className="font-bold text-lg">
+                              Select user role
+                            </h3>
+                            <form onSubmit={handleUserRole}>
+                              <label className="form-control w-full ">
+                                <div className="label">
+                                  <span className="label-text"> Role*</span>
+                                </div>
+                                <select
+                                  // value={newRole}
+                                  // onChange={(e) => setNewRole(e.target.value)}
+                                  className="select select-ghost w-full "
+                                >
+                                  <option value={"guest"}>Guest</option>
+                                  {/* <option value={"guest"}>Guest</option> */}
+                                  <option value={"host"}>Host</option>
+                                  <option value={"admin"}>Admin</option>
+                                </select>
+                              </label>
+                              <div className="flex justify-center mt-5">
+                                <button
+                                  type="submit"
+                                  className="btn btn-success"
+                                >
+                                  Submit
+                                </button>
+                              </div>
+                            </form>
+                          </div>
+                        </dialog>
                       </td>
                     </tr>
                   ))}
